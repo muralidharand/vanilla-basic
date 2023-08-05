40604b310c92a165c1d18305d321585fa0b2c9e9
Muralidharan Deenathayalan <muralidharand@users.noreply.github.com>
2023-08-05 16:55:19 +0530

    Create countdown.js

---
+++ src/countdown.js
@@ -0,0 +1,19 @@
+// Replace the launch date with your desired launch date and time
+const launchDate = new Date("2023-08-31T12:00:00").getTime();
+
+function updateCountdown() {
+  const now = new Date().getTime();
+  const distance = launchDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").textContent = days < 10 ? "0" + days : days;
+  document.getElementById("hours").textContent = hours < 10 ? "0" + hours : hours;
+  document.getElementById("minutes").textContent = minutes < 10 ? "0" + minutes : minutes;
+  document.getElementById("seconds").textContent = seconds < 10 ? "0" + seconds : seconds;
+}
+
+setInterval(updateCountdown, 1000);
